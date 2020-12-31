8d852e992a3c7aac219f2af244128cc6c924392c
hamadmanzari <62907148+hamadmanzari@users.noreply.github.com>
2020-12-31 22:16:51 +0300

    Add files via upload

---
+++ Mad_Libs.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 31 07:10:00 2020
+
+@author: hamad
+"""
+
+adjective1 = input("Enter an adjective: ").strip().lower()
+nationality = input("Enter a nationality: ").title().strip()
+name = input("Enter a person's name: ").title().strip()
+
+
+print("Pizza was invented by a", adjective1, nationality + " chef named", name + '.')
+
